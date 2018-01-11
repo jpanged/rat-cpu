7366b13093e681836b9bd6f0214afa9d56dcd62c
nwang08 <nwang08@calpoly.edu>
2018-01-10 21:50:21 -0800

    made commands lowercase

--- sw1/sw1.asm
+++ sw1/sw1.asm
@@ -5,28 +5,28 @@
 
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;- Register Aliases
-.DEF R_QUEUE = r0 ; register used for holding values
-.DEF R_ADD   = r1 ; register used to add values
+.def R_QUEUE = r0 ; register used for holding values
+.def R_ADD   = r1 ; register used to add values
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;- Assembler Directives
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.EQU INPUTPORT  = 0x30 ; input port for values
-.EQU OUTPUTPORT = 0x40 ; output port for values
+.equ INPUTPORT  = 0x30 ; input port for values
+.equ OUTPUTPORT = 0x40 ; output port for values
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.CSEG
-.ORG  0x00      ; memory location of instruction data
+.cseg
+.org  0x00      ; memory location of instruction data
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-   MOV r0, 0x00 ; queue register
-   MOV r1, 0x00 ; initialize registers (MOV = move value to register)
+   mov r0, 0x00 ; queue register
+   mov r1, 0x00 ; initialize registers (MOV = move value to register)
 
-   IN r0, 0x30 ; grab data, place in r0 (input values are unsigned 8 -bit values)
-   ADD r1, r0   ; take in r0 (source) and add to r1 (destination)
-   IN r0, 0x30
-   ADD r1, r0
-   IN r0, 0x30
-   ADD r1, r0
+   in r0, 0x30 ; grab data, place in r0 (input values are unsigned 8 -bit values)
+   add r1, r0   ; take in r0 (source) and add to r1 (destination)
+   in r0, 0x30
+   add r1, r0
+   in r0, 0x30
+   add r1, r0
 
-   OUT r1, 0x40
+   out r1, 0x40
 
 ; ~~~~~~~~~~~~~~~~~~~ program fragment ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
