52695e7fc75011e7d770fb41234f253b6477ce3b
nwang08 <nwang08@calpoly.edu>
2018-01-18 01:55:21 -0800

    finished sw2_1

--- sw2/sw2_1.asm
+++ sw2/sw2_1.asm
@@ -1,8 +1,3 @@
-; SW 2 Assignment 1
-; CPE 233 Winter 2018
-; Professor Gerfen
-; Russell Caletena, Josiah Pang & Nathan Wang
-;
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;- Port Constants
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,22 +14,26 @@
 .org  0x01 ; memory location of instruction data
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 main:
-    in   r0, 0x30 ; Grab data, place in r0
-    call check_if_gt128
+		in   r0, 0x30 ; Grab data, place in r0
+		call check_if_gt128
 
 check_if_gt128: ; Checks if data greater than 128
-    cmp  r0, xx
-    brne check_if_lt128
-    call div_by4
-    brn  done
-
-check_if_lt128:
-    call mult_by2 ; multiply by two here
+		cmp  r0, 0x80
+		brcc  div_by4 ; If c=0, branch
+		breq  div_by4 ; If z=1, branch
+		call mult_by2
+		brn  done
 
 div_by4:
-    ; divides the input by 4
+		lsr r0
+		clc
+		lsr r0; divides the input by 4
+		brn done
 
 mult_by2:
-    ; multiply the input by 2
+		clc
+		lsl r0
 
 done:
+		out r0, OUTPUT
+		brn main
