f51889da7fa71cbe19f84df933ced82338074eaa
RussellCaletena <RussellCaletena@gmail.com>
2018-01-31 15:52:02 -0800

    Add pseudo code to other ALU cases

--- rat4/rat4.srcs/sources_1/new/rat_alu.vhd
+++ rat4/rat4.srcs/sources_1/new/rat_alu.vhd
@@ -1,36 +1,20 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company: CPE 233 Winter 2018
+-- Engineer: Russell Caletena, Josiah Pang, & Nathan Wang
+--
 -- Create Date: 01/31/2018 02:13:04 PM
--- Design Name: 
+-- Design Name: register file
 -- Module Name: rat_alu - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: RAT Assignment 4
+-- Target Devices: Basys 3
+-- Tool Versions:
+-- Description: Creates an arithmetic logic unit component for the RAT CPU
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL; --import library to allow the use of arithmetic symbols
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity rat_alu is
     Port ( a : in STD_LOGIC_VECTOR (7 downto 0);
@@ -51,7 +35,7 @@ begin
         c_flag <= '0'; --initialize c_flag to '0'
         z_flag <= '0'; --initialize z_flag to '0'
 
-        case sel is 
+        case sel is
             when "0000" => --add instruction
                 v_res := ('0' & a) + ('0' & b); --assign a 0 to the msb of v_res to ensure unsigned ints for both a and b
                 c_flag <= v_res(8);
@@ -62,9 +46,12 @@ begin
                 v_res := ('1' & a) - ('1' & b); --assign a 1 to the msb of v_res to ensure signed ints for both a and b
                 c_flag <= v_res(8);
             when "0011" => --subc instruction
-                v_res := ('1' & a) - ('1' & b); --assign a 1 to the msb of v_res to ensure signed ints for both a and b
+                v_res := ('1' & a) - ('1' & b) - c_in; --assign a 1 to the msb of v_res to ensure signed ints for both a and b
 
             when "0100" => --cmp instruction
+                --pseudo code for cmp
+                --if a - b = 0 => z_flag =1 and c_flag = 0
+                --else => z_glag = 0 and c_flag = 1
             when "0101" => --and instruction
                 v_res := a and b;
             when "0110" => --or instruction
@@ -72,12 +59,38 @@ begin
             when "0111" => --exor instruction
                 v_res := a xor b;
             when "1000" => --test instruction
+                --performs a bit-wise logical AND operation between source and destination operands.
+                --the z flag is altered
             when "1001" => --lsl instruction
+                --shift bits left
+                --c_flag --> lsb
+                --msb --> c_flag
             when "1010" => --lsr instruction
-            when "1011" => --rol instruction
-            when "1100" => --ror instruction
-            when "1101" => --asr instruction
-            when "1110" => --mov instruction
+                --shift bits right
+                --c_flag --> msb
+                --lsb --> c_flag
+            when "1011" => --rol instruction (rotate left)
+                --msb --> c_flag
+                --msb --> lsb
+            when "1100" => --ror instruction (rotate right)
+                --lsb --> c_flag
+                --lsb --> msb
+            when "1101" => --asr instruction (arithmetic shift right)
+                --lsb --> c_flag
+                --msb --> v(7)
+            when "1110" => --mov instruction (mov)
+                
             when "1111" => --unused instruction
-            
+            when others => v_res := (others => '1');
+        end case;
+
+        if (v_res(7 downto 0) = X"00") then --assignment of the Z indicator
+            z_flag <= '1';                  --if the 8-bit result of the ALU
+        end if;                             --operation is zero, this reassigns
+                                            --the Z signal
+
+        sum <= v_res(7 downto 0); --assigns the lower 8-bits of the intermediate
+                                  --result of v_res variable to the output sum
+
+    end process;
 end Behavioral;
