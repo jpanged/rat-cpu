5668677b21147375bb5bfe4b075f703d45bb572c
Josiah Pang <pang.josiah@gmail.com>
2018-02-15 00:39:02 -0800

    fix rat2 naming thing

--- rat2/rat2/rat2.srcs/sources_1/new/rat2.vhd
+++ rat2/rat2/rat2.srcs/sources_1/new/rat2.vhd
@@ -1,21 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 01/23/2018 10:07:48 PM
--- Design Name: 
--- Module Name: rat2 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Company: CPE 233 Winter 2018
+-- Engineer: Russell Caletena, Josiah Pang, & Nathan Wang
+--
+-- Create Date: 01/24/2018 12:07:52 AM
+-- Design Name: PC Wrapper
+-- Module Name: pc_wrapper - Behavioral
+-- Project Name: RAT Assignment 2
+-- Target Devices: Basys 3
+-- Description: Wrapper class that pulls the components for the RAT Program Counter
+
 ----------------------------------------------------------------------------------
 
 
@@ -23,19 +16,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL; -- defines signed / unsigned vectors
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity rat2 is
+entity pc_wrapper is
     Port ( FROM_IMMED : in STD_LOGIC_VECTOR (9 downto 0); -- input to mux_4t1
            FROM_STACK : in STD_LOGIC_VECTOR (9 downto 0); -- input to mux_4t1
-           0x3FF      : in STD_LOGIC_VECTOR (9 downto 0); -- input to mux_4t1
            PC_MUX_SEL : in STD_LOGIC_VECTOR (1 downto 0); -- input to mux_4t1
            PC_LD      : in STD_LOGIC; -- input to PC
            PC_INC     : in STD_LOGIC; -- input to PC
@@ -43,44 +26,48 @@ entity rat2 is
            CLK        : in STD_LOGIC; -- input to PC
            PC_COUNT   : out STD_LOGIC_VECTOR (9 downto 0));
 
-end rat2;
+end pc_wrapper;
 
-architecture Behavioral of rat2 is
+architecture Behavioral of pc_wrapper is
 
-    -- mux_4t1 --
-    component mux_4t1
-        Port ( FROM_IMMED, FROM_STACK, 0x3FF : in STD_LOGIC_VECTOR(9 downto 0);
-               PC_MUX_SEL : in STD_LOGIC_VECTOR (1 downto 0);
-               MUX_OUT : out STD_LOGIC_VECTOR (9 downto 0));
+    -- 4-to-1 10-bit mux --
+    component mux_4to1
+        Port ( sel  : in  std_logic_vector (1 downto 0);
+                  in1 : in  std_logic_vector (9 downto 0);
+                  in2 : in  std_logic_vector (9 downto 0);
+                  in3 : in  std_logic_vector (9 downto 0);
+                  d_out    : out std_logic_vector(9 downto 0));
     end component;
-    
-    -- PC --
+
+    -- Program Counter --
     component pc
-        port ( PC_IN : in STD_LOGIC_VECTOR (9 downto 0);
-               PC_LD : in STD_LOGIC;
-               PC_INC : in STD_LOGIC;
-               RST : in STD_LOGIC;
-               CLK : in STD_LOGIC;
-               PC_COUNT : out STD_LOGIC_VECTOR (9 downto 0));
+        Port ( d_in : in STD_LOGIC_VECTOR (9 downto 0);
+               pc_ld : in STD_LOGIC;
+               pc_inc : in STD_LOGIC;
+               rst : in STD_LOGIC;
+               clk : in STD_LOGIC;
+               pc_count : out STD_LOGIC_VECTOR (9 downto 0));
     end component;
-    
+
     -- intermediate signal declaration --
     signal D_IN : STD_LOGIC_VECTOR (9 downto 0);
 
 begin
-    mux1 : mux_4t1
-    port map ( FROM_IMMED => FROM_IMMED,
-               FROM_STACK => FROM_STACK,
-               0x3FF => 0x3FF,
-               PC_MUX_SEL => PC_MUX_SEL,
-               MUX_OUT => D_IN);
-               
+
+-- Port map components
+    mux1 : mux_4to1
+    port map ( in1 => FROM_IMMED,
+               in2 => FROM_STACK,
+               in3 => (others => '1'), -- Equals 3FF
+               sel => PC_MUX_SEL,
+               d_out => D_IN);
+
     pc1 : pc
-    port map ( PC_IN => D_IN,
-               PC_LD => PC_LD,
-               PC_INC => PC_INC,
-               RST => RST,
-               CLK => CLK,
-               PC_COUNT => PC_COUNT);
-   
+    port map ( d_in => D_IN,
+               pc_ld => PC_LD,
+               pc_inc => PC_INC,
+               rst => RST,
+               clk => CLK,
+               pc_count => PC_COUNT);
+
 end Behavioral;
