76efc7eb8ca1068b0082ad5681b580ea4721ea12
Josiah Pang <pang.josiah@gmail.com>
2018-03-16 23:35:50 -0700

    final project page

--- final_project/assembly program/hoop.asm
+++ final_project/assembly program/hoop.asm
@@ -2,7 +2,10 @@
 ; CPE 233 Winter 2018
 ; Professor Gerfen
 ; Russell Caletena, Josiah Pang & Nathan Wang
-; Description:
+; Description: This simple Assembly program work with our Basketball Shot
+;			   Detector to keep track of shots and output them to the seven
+;			   segment display. It initializes a register and increments on
+;			   every interrupt input.
 ;
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;- Port Constants
@@ -14,16 +17,16 @@
 .cseg
 .org  0x10 ; Memory location of instruction data
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	sei
-    mov r0, 0x00
+	sei ; Enable interrupts
+    mov r0, 0x00 ; Initialize count register
 main:
-	out r0, sseg
+	out r0, sseg ; Output current value
 	brn main ; Loop
 
 .org 0x20
-isr:
-	add r0, 0x01
-    retie
+isr: ; Runs on an interrupt
+	add r0, 0x01 ; Increment counter
+    retie ; Return with interrupt enabled
 
 .org 0x3FF
 	brn isr
